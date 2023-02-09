673250e9a087034b25a17bde80c88caeb3011fc8
ok <kelsey.collins23@d155.org>
2023-02-09 07:30:23 -0600

    Initial Commit

---
+++ GroupApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupApp
+//
+//  Created by KELSEY COLLINS on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
