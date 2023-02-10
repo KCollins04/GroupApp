cf069ff61eaddb7aa76c497f30bf0990894e7618
ok <kelsey.collins23@d155.org>
2023-02-10 08:06:49 -0600

    Finish set up for TableView

--- GroupApp/ViewController.swift
+++ GroupApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var restorantInputOutlet: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getCurrentLocationAction(_ sender: Any) {
+    }
+    
+    
+    
 }
 
