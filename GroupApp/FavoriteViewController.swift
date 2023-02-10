932de0798f9ee46446a52673ad83d5c2c8580f60
ok <kelsey.collins23@d155.org>
2023-02-10 07:57:19 -0600

    TableView Setup finish

--- GroupApp/FavoriteViewController.swift
+++ GroupApp/FavoriteViewController.swift
@@ -7,23 +7,28 @@
 
 import UIKit
 
-class FavoriteViewController: UIViewController {
+class FavoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewOutlet.dataSource = self
+        tableViewOutlet.delegate = self
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+  
 
 }
