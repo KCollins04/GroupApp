08ddcb70eb6ab3d27a62e3354810ec5e7305c829
ok <kelsey.collins23@d155.org>
2023-02-16 08:09:52 -0600

    Current Location Works. Fix the region and search.

--- GroupApp/ViewController.swift
+++ GroupApp/ViewController.swift
@@ -8,33 +8,59 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var restorantInputOutlet: UITextField!
     @IBOutlet weak var mapOutlet: MKMapView!
+    var locationManager = CLLocationManager()
+    var current : CLLocation!
     
+    var restorant : [MKMapItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
         
-        mapOutlet.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         
-        // Do any additional setup after loading the view.
-  
-        Task {
-          do{
-                try await print(getMenu("513fbc1283aa2dc80c000021"))
-            } catch{
-                print("Unknown error")
-            }
+        // shows your location
     
-        }
+        mapOutlet.showsUserLocation = true
+        
+        //Current location of the user
+        let center = locationManager.location!.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapOutlet.setRegion(region, animated: true)
         
     }
 
    
     
     
+    @IBAction func rearchAction(_ sender: UIBarButtonItem) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = restorantInputOutlet.text
+        let span = MKCoordinateSpan(
+            latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: current.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response
+            else{return}
+            for mapItem in response.mapItems{
+                self.restorant.append(mapItem)
+                let annoatation = MKPointAnnotation()
+                annoatation.coordinate = mapItem.placemark.coordinate
+                annoatation.title = mapItem.name
+                self.mapOutlet.addAnnotation(annoatation)
+                
+            }
+        }
+    }
     
 }
 
