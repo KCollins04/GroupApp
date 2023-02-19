01931e35279fcb4a797f682f077c0c725825012a
Tim Collins <tim@redcranial.com>
2023-02-19 16:39:06 -0600

    Set up UIAnnotation and UIAlart.

--- GroupApp/ViewController.swift
+++ GroupApp/ViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
 
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     @IBOutlet weak var restorantInputOutlet: UITextField!
     @IBOutlet weak var mapOutlet: MKMapView!
     var locationManager = CLLocationManager()
@@ -26,7 +27,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         // shows your location
-    
+        
         mapOutlet.showsUserLocation = true
         
         
@@ -40,7 +41,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let region = MKCoordinateRegion(center: center, span: span)
         mapOutlet.setRegion(region, animated: true)
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showMenu" {
             let controller = segue.destination as! PickerViewController
@@ -53,8 +54,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
-
-   
+    
+    
     
     
     @IBAction func rearchAction(_ sender: UIBarButtonItem) {
@@ -66,22 +67,62 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                     print(item.name,item.lat,item.lng,item.id)
                     let location = CLLocation(latitude: item.lat,longitude: item.lng)
                     let newPin = MKPointAnnotation()
-                        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-                        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-
-                        //set region on the map
-                        mapOutlet.setRegion(region, animated: true)
-
-                        newPin.coordinate = location.coordinate
+                    let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                    let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                    
+                    //set region on the map
+                    mapOutlet.setRegion(region, animated: true)
+                    
+                    newPin.coordinate = location.coordinate
                     newPin.title = item.name
-                        mapOutlet.addAnnotation(newPin)
+                    mapOutlet.addAnnotation(newPin)
                 }
                 
             } catch{
                 print(error)
             }
         }
+        
+        
+        // Getting the name of the place and adding info at the top of the pin  https://www.youtube.com/watch?v=VkvEA7v5JyQ&list=PLuoeXyslFTuas6GrfsUiFPShGXmaVDbgN&index=112
+        
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation)-> MKAnnotationView?{
+            guard annotation is _____ else{return nil}
+            
+            let identifier = "___"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil{
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                
+                let btn = UIButton(type: .detailDisclosure)
+                annotationView?.rightCalloutAccessoryView = btn
+            }
+            else{
+                annotationView?.annotation = annotation
+            }
+            return annotationView
+        }
+        
+        
+        //UIAlert where it will take the user to another location when they accept https://stackoverflow.com/questions/54553460/performing-segue-after-uialert
+        
+        
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl){
+            guard let _____ = view.annotation as? _____ else{return}
+            
+            let option = UIAlertAction(title: "Would you like to order from this place?", style: .default, handler: {(action) -> Void in
+                let vc = self.storyboard.instan... //get the destination view controller
+                    self.navigationController.push(vc, animated: true) //or you can present it using self.present(...) method
+
+            })
+        }
+        
+        
+        
+        
     }
     
 }
-
