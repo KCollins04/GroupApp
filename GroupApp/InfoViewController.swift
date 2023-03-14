eb285ef81e459c2be5328d26b867ee98907d33bc
ok <kelsey.collins23@d155.org>
2023-03-14 08:09:05 -0500

    info done

--- GroupApp/InfoViewController.swift
+++ GroupApp/InfoViewController.swift
@@ -12,7 +12,6 @@ class InfoViewController: UIViewController {
     @IBOutlet weak var itemNameOutlet: UILabel!
     @IBOutlet weak var servingTextOutlet: UITextField!
 
-    
     @IBOutlet weak var servinfSize: UILabel!
     @IBOutlet weak var totalFat: UILabel!
     @IBOutlet weak var caloriesLabel: UILabel!
@@ -26,6 +25,11 @@ class InfoViewController: UIViewController {
     @IBOutlet weak var proteinLabel: UILabel!
     @IBOutlet weak var sugarLabel: UILabel!
     
+    @IBOutlet weak var ironLabel: UILabel!
+    @IBOutlet weak var calciumLabel: UILabel!
+    @IBOutlet weak var vitaminCLabel: UILabel!
+    @IBOutlet weak var vitaminALabel: UILabel!
+    
     var itemInfo: (foodItem,itemInfo)!
     
     override func viewDidLoad() {
@@ -33,11 +37,19 @@ class InfoViewController: UIViewController {
         itemNameOutlet.text = itemInfo.0.item_name
         servinfSize.text = "\(itemInfo.0.serving_qty!)"
         caloriesLabel.text = "\(itemInfo.1.nf_calories!)"
-        tFat.text = "\(itemInfo.1.nf_total_fat!)"
-        sFat.text = "\(itemInfo.1.nf_saturated_fat!)"
-        tFat.text = "\(itemInfo.1.nf_trans_fatty_acid!)"
-        cholesterolLabel.text = "\(itemInfo.1.nf_cholesterol!)"
-        sodiumLabel.text = "\(itemInfo.1.nf_sodium!)"
+        totalFat.text = "\(itemInfo.1.nf_total_fat!)g"
+        sFat.text = "\(itemInfo.1.nf_saturated_fat!)g"
+        tFat.text = "\(itemInfo.1.nf_trans_fatty_acid!)g"
+        cholesterolLabel.text = "\(itemInfo.1.nf_cholesterol!)mg"
+        sodiumLabel.text = "\(itemInfo.1.nf_sodium!)g"
+        totalCarbohydrateLabel.text = "\(itemInfo.1.nf_total_carbohydrate!)g"
+        dieFiberLabel.text = "\(itemInfo.1.nf_dietary_fiber!)g"
+        sugarLabel.text = "\(itemInfo.1.nf_sugars!)g"
+        proteinLabel.text = "\(itemInfo.1.nf_protein!)g"
+        vitaminALabel.text = "\(itemInfo.1.nf_vitamin_a_dv!)mcg"
+        vitaminCLabel.text = "\(itemInfo.1.nf_vitamin_c_dv!)mcg"
+        ironLabel.text = "\(itemInfo.1.nf_iron_dv!)mg"
+        calciumLabel.text = "\(itemInfo.1.nf_calcium_dv!)mg"
 
 
     }
@@ -46,11 +58,23 @@ class InfoViewController: UIViewController {
     @IBAction func addAction(_ sender: UIBarButtonItem) {
         let serving = Double(servingTextOutlet.text!)
         
-        let cal = serving! * itemInfo.0.calories
-        let ser = serving! * itemInfo.0.serving_qty!
-        let totalf = serving! * itemInfo.1.nf_total_fat!
-      /*  infoTextOutlet.text = "Calories: \(cal)\nServing Quantity: \(ser) \(itemInfo.0.serving_unit!)\nTotal Fat: \(totalf)%"
-*/
+        itemNameOutlet.text = itemInfo.0.item_name
+        servinfSize.text = "\(serving! * itemInfo.0.serving_qty!)"
+        caloriesLabel.text = "\(serving! * itemInfo.1.nf_calories!)"
+        totalFat.text = "\(serving! * itemInfo.1.nf_total_fat!)g"
+        sFat.text = "\(serving! * itemInfo.1.nf_saturated_fat!)g"
+        tFat.text = "\(serving! * itemInfo.1.nf_trans_fatty_acid!)g"
+        cholesterolLabel.text = "\(serving! * itemInfo.1.nf_cholesterol!)mg"
+        sodiumLabel.text = "\(serving! * itemInfo.1.nf_sodium!)g"
+        totalCarbohydrateLabel.text = "\(serving! * itemInfo.1.nf_total_carbohydrate!)g"
+        dieFiberLabel.text = "\(serving! * itemInfo.1.nf_dietary_fiber!)g"
+        sugarLabel.text = "\(serving! * itemInfo.1.nf_sugars!)g"
+        proteinLabel.text = "\(serving! * itemInfo.1.nf_protein!)g"
+        vitaminALabel.text = "\(serving! * itemInfo.1.nf_vitamin_a_dv!)mcg"
+        vitaminCLabel.text = "\(serving! * itemInfo.1.nf_vitamin_c_dv!)mcg"
+        ironLabel.text = "\(serving! * itemInfo.1.nf_iron_dv!)mg"
+        calciumLabel.text = "\(serving! * itemInfo.1.nf_calcium_dv!)mg"
+   
         
     }
     
